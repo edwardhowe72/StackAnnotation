bc08ff7674febcb5567ad09af9a297949d206f71
Howe Chin Kheong  (NCS) <chinkheong@ncs.com.sg>
2020-04-28 17:15:28 +0800

    fail the test case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -35,6 +35,7 @@ public class MyStringStackTest extends TestCase {
 		String o1 = null;  
 		MyStringStack stack = new MyStringStack();
                 stack.push (s1);
+
                 assertTrue (stack.isEmpty());
 
                 //assertTrue (stack.isEmpty());
